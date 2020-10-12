b54c2540cc59d50933ee5682cee67f5f322b6696
Andreea <andrea.iosip@gmail.com>
2020-10-12 12:29:34 +0000

    Project name changed

--- thekettle/asgi.py
+++ thekettle/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'camera_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the-kettle.settings')
 
 application = get_asgi_application()
